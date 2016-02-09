13de166bba7288dd0f4b3e2d02cfa7c4afdb8f40
davidmoradi <david@wishpond.com>
2016-02-08 17:51:59 -0800

    Integration test for creating catergory

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,28 @@
 class CategoriesController < ApplicationController
-  def index
-  end
+	def index
+		@categories = Category.all
+	end
 
-  def new
-  end
+	def new
+		@category = Category.new
+	end
 
-  def show
-  end
+	def create
+		@category = Category.new(category_params) 
+		
+		if @category.save
+			flash[:success] = "category was created successfully"
+			redirect_to categories_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+	end
+
+	private
+	def category_params
+		params.require(:category).permit(:name)
+	end
 end
